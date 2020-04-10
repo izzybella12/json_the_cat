4d1c48a50d6ad7ac776694dd58a5d4fca626ede7
Isabella Panagrosso <isabellapanagrosso15@gmail.com>
2020-04-10 03:09:55 +0000

    asynch and callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,29 @@
 const request = require('request');
-const fetchBreedDescription = require('./index');
 
-let preliminaryCatBreed = process.argv;
-let catBreed = process.argv.splice(2);
-let siberianCat = "https://api.thecatapi.com/v1/breeds/search?q=" + catBreed;
+const fetchBreedDescription = function(breedName, callback) {
 
-if (catBreed.length === 0) {
-  console.log("Which breed are you looking for?");
-} else {
-  request(siberianCat, (error, response, body) => {
-    if (error) {
-      console.log('error', error);
-    } else {
-      console.log('statusCode:', response && response.statusCode);
-      const data = JSON.parse(body);
-      let firstObject = data[0];
-      let description = firstObject['description'];
-      console.log(description);
-    }
-  });
-}
+  let siberianCat = "https://api.thecatapi.com/v1/breeds/search?q=" + breedName;
+
+  if (!breedName) {
+    callback("Which breed are you looking for?");
+  } else {
+    request(siberianCat, (error, response, body) => {
+      if (error) {
+        callback(error);
+      } else {
+        const data = JSON.parse(body);
+        let firstObject = data[0];
+        let description = firstObject['description'];
+        callback(null, description);
+      }
+    });
+  }
+};
+
+module.exports = fetchBreedDescription;
+
+
+
 
 
 
